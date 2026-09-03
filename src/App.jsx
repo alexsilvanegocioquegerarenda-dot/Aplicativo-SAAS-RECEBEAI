8d1ef002251a0c1bc6cd33a857ef1447da0b38f1
alemarcal2015-bit <alemarcal2015@gmail.com>
2026-09-03 15:00:40 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react'
-import Configuracoes from './pages/Configuracoes';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Importações das páginas (verifique se todas usam "export default" em seus arquivos)
+import Dashboard from './pages/Dashboard';
+import Clientes from './pages/Clientes';
 import ClienteDetalhes from './pages/ClienteDetalhes';
+import Recebiveis from './pages/Recebiveis';
+import Configuracoes from './pages/Configuracoes';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/configuracoes" replace />} />
-        <Route path="/configuracoes" element={<Configuracoes />} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/clientes" element={<Clientes />} />
         <Route path="/clientes/:id" element={<ClienteDetalhes />} />
+        <Route path="/recebiveis" element={<Recebiveis />} />
+        <Route path="/configuracoes" element={<Configuracoes />} />
       </Routes>
     </BrowserRouter>
   );
